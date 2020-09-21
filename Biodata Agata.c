9c585bfe20f6f1ef3fbbf449b39ef53e96adbc93
ayuwidyaagata <ayuwagata@gmail.com>
2020-09-21 19:10:11 +0700

    first commit

---
+++ Biodata Agata.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+printf("Nama \t\t: Ayu Widya Agata\n");
+printf("NPM \t\t: 19081010005\n");
+printf("TTD  \t\t: Lamongan, 18 Maret 2001\n");
+printf("Alamat \t\t: Jl. Medokan Sawah Timur Gg 4c no. 3\n");
+printf("Jenis Kelamin \t: Perempuan\n");
+printf("Statuus \t: Belum Kawin\n");
+printf("Kewarganegaraan : Indonesia\n");
+printf("Telp \t\t: 085335989968\n");
+printf("Email \t\t: ayuwagata@gmail.com\n");
+}
+
